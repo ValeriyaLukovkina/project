5858a93c1f8f11fcec6a80409dfe9cb1809a8d03
Valeriya <valeriyalukovkina@gmail.com>
2022-06-27 01:28:42 +0800

    5 block

--- main.js
+++ main.js
@@ -62,7 +62,7 @@
         close.classList.add('fixed');
         close.addEventListener('click', function(event) {
             successSend.classList.add('success_send_disable');
-            top.classList.remove('top_mute');
+            wrp.classList.remove('top_mute');
             body.removeChild(close)
         })
         body.appendChild(close);
@@ -97,11 +97,12 @@
         successSend.classList.add('success_send_disable');
         top.classList.remove('top_mute');
         body.removeChild(close);
+        // body.classList.remove('top_mute')
         wrp.classList.remove('top_mute')
     })
 })('#top_block_form_sub', '.top_block_form', '.success_send', '.success_send_button', '.top', '.wrp');
 
-;(function getTreatment (selectorService, selectorServicePrice) {
+;(function getTreatment (selectorService, selectorServicePrice, selectorWrp) {
     let servicePrice = document.querySelector(selectorServicePrice);
     let service = document.querySelector(selectorService);
     let wrp = service.querySelector('.service_wrp_in');
@@ -109,29 +110,96 @@
     let servicePriceClose = servicePrice.querySelector('.service_button_close');
     let fixed;
     let serviceButton = servicePrice.querySelector('.service_button');
+    let body = document.querySelector('body')
     for (let buttonShowPrice of buttonsShowPrice) {
         buttonShowPrice.addEventListener('click', function() {
             event.preventDefault();
             servicePrice.classList.remove('service_price_treatment_wrp_disable');
             wrp.classList.add('top_mute');
+            // servicePrice.classList.add('service_price_treatment_wrp_add')
+            // wrpAll.classList.add('top_mute');
             fixed = document.createElement('div');
-            fixed.classList.add('fixed');
+            fixed.classList.add('fixed-1');
             fixed.addEventListener('click', function() {
                 servicePrice.classList.add('service_price_treatment_wrp_disable');
                 wrp.classList.remove('top_mute');
-                wrp.removeChild(fixed);
+                servicePrice.removeChild(fixed);
             });
-            wrp.appendChild(fixed);
+            servicePrice.appendChild(fixed);
         });
     }
     serviceButton.addEventListener('click', function() {
-        wrp.removeChild(fixed);
+        servicePrice.removeChild(fixed);
         wrp.classList.remove('top_mute');
+        // wrpAll.classList.remove('top_mute');
     })
     servicePriceClose.addEventListener('click', function() {
         servicePrice.classList.add('service_price_treatment_wrp_disable');
         wrp.classList.remove('top_mute');
-        wrp.removeChild(fixed);
+        servicePrice.removeChild(fixed);
     });
 
-})('.service_wrp', '.service_price_treatment_wrp');
+})('.service_wrp', '.service_price_treatment_wrp', '.wrp');
+
+;(function getPhoto (selectorPortfolio) {
+    let portfolio = document.querySelector(selectorPortfolio);
+    let portfolioScrol = portfolio.querySelector('.portfolio_scroll');
+    let portfolioPhotoBefore = portfolio.querySelector('.portfolio_photo_before');
+    let portfolioPhotoAftrer = portfolio.querySelector('.portfolio_photo_after');
+    let portfolioPhotoWrp = portfolio.querySelector('.portfolio_photo_wrp');
+    let width = 500
+    portfolioScrol.addEventListener ('mousedown', function(e) {
+        e.preventDefault();
+        portfolioScrol.style.pointerEvents = 'none';
+        portfolioPhotoWrp.addEventListener('mousemove', getMove)
+    });
+
+    portfolioPhotoWrp.addEventListener ('mouseup', function(e) {
+        e.preventDefault();
+        portfolioScrol.style.pointerEvents = 'all';
+        portfolioPhotoWrp.removeEventListener('mousemove', getMove)
+    });
+
+    portfolioScrol.addEventListener('click', function() {
+        event.preventDefault();
+    })
+
+    function getMove(e) {
+        portfolioScrol.style.left = e.layerX + 'px';
+        portfolioPhotoBefore.style.clip = 'rect(0px,500px,400px,' + e.layerX + 'px)'; 
+        portfolioPhotoAftrer.style.clip = 'rect(0px,' + e.layerX + 'px,400px, 0)'; 
+    }
+
+})('.portfolio');
+
+;(function getSliderPhotoDoctor(selectorDoctor) {
+    let doctor = document.querySelector(selectorDoctor);
+    let slidesWrp = doctor.querySelector('.doctor_block_wrp')
+    let slides = doctor.querySelectorAll('.doctor_block');
+    let buttonPrev = doctor.querySelector('.doctor_slide_prev_wrp');
+    let buttonNext = document.querySelector('.doctor_slide_next_wrp');
+    let i = 1;
+    let k = slides.length;
+        buttonNext.addEventListener('click', function() {
+            if (i > slides.length - 4) {
+                return i = 1;
+            }
+            let lastChild = doctor.querySelector('.doctor_block_wrp .doctor_block:nth-child(' + slides.length + ')');
+            let childPrev = doctor.querySelector('.doctor_block_wrp .doctor_block:nth-child(' + i + ')');
+            let childNext = doctor.querySelector('.doctor_block_wrp .doctor_block:nth-child(' + (4 + i) + ')');
+            childPrev.classList.add('doctor_block_disable');
+            childNext.classList.remove('doctor_block_disable');
+            i++;
+        });
+        buttonPrev.addEventListener('click', function() {
+            if (k <=  4) {
+                return k = slides.length;
+            }
+            let childPrev = doctor.querySelector('.doctor_block_wrp .doctor_block:nth-child(' + (k - 4) + ')');
+            let childNext = doctor.querySelector('.doctor_block_wrp .doctor_block:nth-child(' + (k) + ')');
+            childPrev.classList.remove('doctor_block_disable');
+            childNext.classList.add('doctor_block_disable');
+            k--;
+        });
+})('.doctor');
+
