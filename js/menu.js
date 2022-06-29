128708ca1ccccad9a3ccc178474383219265f667
Valeriya <valeriyalukovkina@gmail.com>
2022-06-29 21:51:05 +0800

    processing

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+;(function openMenu(selectorTop) {
+    let top = document.querySelector(selectorTop);
+    let btn = top.querySelector('.top_nav_btn');
+    btn.addEventListener('click', function() {
+        
+    })
+})('.top');
